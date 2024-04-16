4bc987a6754118c89a48fffc0586c5a1e328be15
Nitesh Kumar <niteshkumar.rv@theproindia.com>
2024-04-16 10:07:00 +0530

    Add project files.

---
+++ ApiProject/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using ApiProject.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProject.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        [Route("GetData")]
+        public string GetData()
+        {
+            return "Authenticated with JWT";
+        }
+
+        [HttpGet]
+        [Route("Details")]
+        public string Details()
+        {
+            return "Authenticated with JWT";
+        }
+
+        [Authorize]
+        [HttpPost]
+        public string AddUser(User user)
+        {
+            return "User added with username " + user.Username;
+        }
+    }
+}
---
+++ ApiProject/Data/AllContext.cs
@@ -0,0 +1,14 @@
+﻿
+using ApiProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiProject.Data
+{
+    public class AllContext : DbContext
+    {
+        public AllContext(DbContextOptions<AllContext> options) : base(options) { }
+
+        public virtual DbSet<Users> Users { get; set; }
+        //public virtual DbSet<User> User { get; set; }
+    }
+}
